d00da9b238507db45e6e3584962f0cdbf83fb9ec
haos-bsquare <haos@bsquare.com>
2017-01-11 13:36:18 +0000

    Minor changes to make model works.
    Signed-off-by: haos-bsquare <haos@bsquare.com>

--- Route.py
+++ Route.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python
 
+import numpy as np
+
 class Route:
-    def __init__(self, dist, wTime, bTime):
+    def __init__(self, dist, wTime, bTime, MLPredictAcceptableRate=False):
         self.mDistance = dist
         self.mWalkTime = wTime
         self.mBusTime = bTime
+        if MLPredictAcceptableRate:
+            from keras.models import load_model
+            self.mModel = load_model("RouteModel.h5")
+            self.mModel.load_weights("RouteModelWeights.h5")
 
+    # This function requires MLPredictAcceptableRate to be true.
     def getAcceptablePossibility(self):
-        return 0.5
+        return self.mModel.predict(np.array([[self.mDistance, self.mWalkTime, self.mBusTime]]))[0][0]
 
+    # This function requires MLPredictAcceptableRate to be true.
     def getAcceptable(self):
         return self.getAcceptablePossibility() >= 0.5
 
+    # This function requires MLPredictAcceptableRate to be true.
     def __lt__(self, other):
         return self.getAcceptablePossibility() < other.getAcceptablePossibility()
+    # This function requires MLPredictAcceptableRate to be true.
     def __gt__(self, other):
         return self.getAcceptablePossibility() > other.getAcceptablePossibility()
 
